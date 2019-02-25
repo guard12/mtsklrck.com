ad7d1f303dada9179cac36a8a4c96f5ec445afee
Matt <mko@reviso.com>
2019-02-25 13:48:42 +0100

    Small changes to projects

--- components/FeaturedProjects.js
+++ components/FeaturedProjects.js
@@ -1,5 +1,8 @@
-import { Card, Icon } from 'semantic-ui-react'
+import { Card, Icon, List } from 'semantic-ui-react'
 
+const style = {
+    color: 'black'
+}
 
 const FeaturedProjects = (props) => (
     <Card.Group stackable itemsPerRow={3}>
@@ -8,8 +11,14 @@ const FeaturedProjects = (props) => (
         target='_blank'
         color='red'
     >
-        <Card.Content header='dronerepublic.sk' />
-        <Card.Content description='E-shop focused on selling drones and cameras for professionals' />
+        <Card.Content header='Dronerepublic.sk'/>
+        <Card.Content textAlign='left' description={
+            <List size='medium' style={style} bulleted>
+                <List.Item content='E-shop focused on selling drones and cameras for professionals and amateurs'/>
+                <List.Item content='We are running the business in collaboration with my brother who is the business owner'/>
+                <List.Item content='As it is our first project, we mainly focus on learning'/>
+            </List>
+        }/>
         <Card.Content extra>
         <Icon name='time' />
         2016 - Present
@@ -21,8 +30,15 @@ const FeaturedProjects = (props) => (
         target='_blank'
         color='violet'
     >
-        <Card.Content header='pilothub.com' />
-        <Card.Content description='Platform for professional drone pilots and cameramen using ReactJS and Firebase' />
+        <Card.Content header='Pilothub.com' />
+        <Card.Content textAlign='left' description={
+            <List size='medium' style={style} bulleted>
+                <List.Item content='Platform for professional drone pilots and cameramen'/>
+                <List.Item content='However this platform should fill the void on the market between drone pilots and their customers'/>
+                <List.Item content='The application is developed with ReactJS and Firebase'/>
+                <List.Item content='The release date is set to be in first half of 2019'/>
+            </List>
+        }/>
         <Card.Content extra>
         <Icon name='time' />
         2018 - Present
@@ -30,12 +46,17 @@ const FeaturedProjects = (props) => (
     </Card>
 
     <Card
-        href='https://www.mtsklrck.com'
+        href='https://www.mtsklrck.now.sh'
         target='_blank'
         color='blue'
     >
         <Card.Content header='mtsklrck.com' />
-        <Card.Content description='My Personal website project using next.js and ReactJS' />
+        <Card.Content textAlign='left' description={
+            <List size='medium' style={style} bulleted>
+                <List.Item content='My Personal server-side rendered website project using lovely next.js and ReactJS'/>
+                <List.Item content='This website should act as my portfolio and is supposed to represent me and my skills'/>
+            </List>
+        } />
         <Card.Content extra>
         <Icon name='time' />
         2019 - Present
--- components/Projects.js
+++ components/Projects.js
@@ -1,10 +1,31 @@
-import { Grid, Icon } from 'semantic-ui-react'
+import { Grid, Icon, Container, Divider } from 'semantic-ui-react'
 import FeaturedProjects from './FeaturedProjects'
 
 const ProjectsComponent = (props) => (
     <Grid.Row textAlign='center'>
         <Grid.Column width={10} textAlign='center'>
             <h2 style={{display: "inline-block", marginBottom: "30px"}}><Icon name="trophy" color="yellow"/> My personal projects</h2>
+        </Grid.Column>
+
+        <Grid.Column width={8}>
+                <Container>
+                    <p style={{fontSize: "16px"}}>
+                    Here is a list of the projects which I have either developed myself or the projects I contributed to. The list is quite short now,
+                    but hopefully as the months pass this list will grow <Icon name='chart line'/>.
+                    </p>
+                </Container>
+        </Grid.Column>
+
+        <Grid.Column width={10}>
+            <Divider horizontal style={{paddingTop: '5vh', paddingBottom: '5vh'}}>
+                <h3>
+                <Icon name='idea' color='violet' style={{paddingRight: "25px"}}/>
+                Projects
+                </h3>
+            </Divider>
+        </Grid.Column>
+
+        <Grid.Column width={10} textAlign='center'>
             <FeaturedProjects />
         </Grid.Column>
     </Grid.Row>
