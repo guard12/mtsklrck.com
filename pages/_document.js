8c8e778d93c13ed50ba7f07a68bdeea18dd6c689
guard12 <matus.kollarcik@gmail.com>
2020-09-20 18:20:28 +0200

    fix package lock and font

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Document, {
-  Html, Main, NextScript, Head,
+  Html, Main, NextScript,
 } from 'next/document'
 import { ServerStyleSheets } from '@material-ui/core/styles'
 
@@ -8,12 +8,6 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <title>Matus Kollarcik</title>
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-          <link rel="preload" href="/break-extralight.ttf" as="font" crossOrigin="" />
-        </Head>
         <body>
           <Main />
           <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
           backgroundColor: theme.palette.background.main, 
           height: "100vh",
         }}>
-        <div style={{ fontFamily: "Break", paddingLeft: "20%", paddingTop: "20px", fontSize: "40px" }}>MK</div>
+        <div style={{ fontFamily: "Break", paddingLeft: "20%", paddingTop: "20px", fontSize: "3em" }}>MK</div>
        <Card />
        <footer style={{bottom: "20px", position: "fixed", width: "100%"}}>
          <div style={{textAlign: "center" }}> <Typography variant="body2">"When something is important enough, you do it even if the odds are not in your favor." - Elon Musk</Typography>
