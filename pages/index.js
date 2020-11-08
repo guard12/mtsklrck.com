26a661ffefea4f840ba4e9e9da08250b6e7e54e9
Matus Kollarcik <matus.kollarcik@gmail.com>
2020-11-08 13:07:10 +0100

    Add animation to logo

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useTheme } from '@material-ui/core/styles'
 import { Typography, Grid } from '@material-ui/core'
 
 import { Card } from '../src/card'
-//import { Logo } from '../src/logo'
+import { Logo } from '../src/logo'
 
 export default function Home() {
   const theme = useTheme()
@@ -13,7 +13,8 @@ export default function Home() {
           backgroundColor: theme.palette.background.main, 
           height: "100vh",
         }}>
-        <div style={{ fontFamily: "Break", paddingLeft: "20%", paddingTop: "20px", fontSize: "3em" }}>MK</div>
+        <div style={{paddingLeft: "20%", paddingTop: "20px" }}><Logo /></div>
+        
        <Card />
         <footer style={{bottom: "20px", position: "fixed", width: "100%", padding: "10px"}}>
           <div style={{textAlign: "center" }}> <Typography variant="body2">"When something is important enough, you do it even if the odds are not in your favor." - Elon Musk</Typography>
