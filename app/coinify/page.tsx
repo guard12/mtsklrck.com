34039c955d1e7bac47414c8e866d68da94ba0c5e
Matus Kollarcik <mko@coinify.com>
2024-06-03 10:44:26 +0200

    use column

--- app/coinify/page.tsx
+++ app/coinify/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Coinify() {
   return (
-    <div className='flex'>
+    <div className='flex flex-col'>
       <h2 className='text-2xl text-white'>Coinify in iframe</h2>
       <iframe
         src="https://checkout.sandbox.coinify.com/demo/payment"
