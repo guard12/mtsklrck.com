0362940e2cee89d4dbe25435dcf21477f89903e8
Matus Kollarcik <mko@coinify.com>
2024-12-01 13:21:22 +0100

    Add beerleague.net

--- components/home-sections/section-two.tsx
+++ components/home-sections/section-two.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import React from 'react'
+import Link from 'next/link'
 import { motion } from 'framer-motion'
 
 export const SectionTwo = () => {
 
   return (
-    <div className="flex justify-center h-5/6 overflow-hidden mt-20 sm:mt-0">
+    <div className="flex justify-center h-5/6 mt-20 sm:mt-0 overflow-hidden">
       <div
         className="flex sm:w-6/12 w-11/12 sm:justify-evenly items-center flex-col sm:flex-row gap-20 sm:gap-0"
       >
@@ -36,13 +37,31 @@ export const SectionTwo = () => {
           transition={{ duration: 1 }}
         />
         <motion.div
-          className="flex sm:w-5/12 w-11/12 sm:py-24 sm:justify-start justify-center overflow-hidden"
+          className="flex mb-52 sm:w-5/12 w-11/12 sm:justify-start justify-center overflow-hidden"
           initial={{ x: '100vw' }}
           animate={{ x: 0 }}
           transition={{ type: 'tween',
             delay: 0.5,
             duration: 0.5 }}
         >
+          <div className="text-white border border-red-800 p-5 rounded-xl gap-6 flex flex-col">
+            <h3 className="text-bold">Side project</h3>
+            <div className="flex flex-col gap-1">
+              <label className="text-sm">Creator</label>
+              <Link
+                aria-label="Beerleague.net"
+                href="https://beerleague.net"
+                target="_blank"
+                referrerPolicy="no-referrer"
+                rel="noreferrer"
+                className="text-blue-500 text-sm hover:underline"
+              >
+                Beerleague.net
+              </Link>
+              <div className="font-thin text-xs text-gray-400">Nov 2024 - Present</div>
+            </div>
+            <label className="text-sm font-thin">Organization of Beer league games, leagues and tournaments</label>
+          </div>
         </motion.div>
       </div>
     </div>
